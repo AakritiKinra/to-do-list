7e04f2e8fa6f18ada79e2af5c442742ef7c21a51
Aakriti Kinra <aakritikinra@gmail.com>
2020-12-28 00:43:14 +0530

    minor changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
